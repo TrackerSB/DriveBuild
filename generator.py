1018450110adc14e1dcc6e0bc08749c9f30dced5
Stefan Huber <huberst@fim.uni-passau.de>
2019-05-09 17:37:30 +0200

    drivebuild: Add return types

--- generator.py
+++ generator.py
@@ -12,21 +12,21 @@ class ScenarioBuilder:
         self.obstacles = obstacles
         self.participants = participants
 
-    def add_lanes_to_scenario(self, scenario: Scenario):
+    def add_lanes_to_scenario(self, scenario: Scenario) -> None:
         for lane in self.lanes:
             road = Road('track_editor_C_center')  # FIXME Maybe change road material
             road.nodes.extend([(lp.position[0], lp.position[1], lp.width) for lp in lane])
             scenario.add_road(road)
 
-    def add_obstacles_to_scenario(self, scenario: Scenario):
+    def add_obstacles_to_scenario(self, scenario: Scenario) -> None:
         for obstacle in self.obstacles:
             pass  # FIXME Not implemented yet
 
-    def add_participants_to_scenario(self, scenario: Scenario):
+    def add_participants_to_scenario(self, scenario: Scenario) -> None:
         for participant in self.participants:
             pass  # FIXME Not implemented yet
 
-    def add_all(self, scenario: Scenario):
+    def add_all(self, scenario: Scenario) -> None:
         self.add_lanes_to_scenario(scenario)
         self.add_obstacles_to_scenario(scenario)
         self.add_participants_to_scenario(scenario)
