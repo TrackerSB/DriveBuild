917a1795fd46a8ff34034518df006c7c0d2388e3
Stefan Huber <huberst@fim.uni-passau.de>
2019-05-13 08:52:22 +0200

    drivebuild: Add FIXME for solving #10

--- generator.py
+++ generator.py
@@ -50,6 +50,7 @@ class ScenarioBuilder:
         for lane in self.lanes:
             road = Road('a_asphalt_01_a')  # FIXME Maybe change road material
             road = Road('track_editor_C_center')  # FIXME Maybe change road material
+            # FIXME Add "overObjects = 1" to prefab
             road_nodes = [(lp.position[0], lp.position[1], 0, lp.width) for lp in lane.nodes]
             road.nodes.extend(road_nodes)
             scenario.add_road(road)
