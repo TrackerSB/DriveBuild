5b0c9e78176778c89a44bfc923a70141b34e0139
Stefan Huber <huberst@fim.uni-passau.de>
2019-05-13 09:25:15 +0200

    drivebuild: Add not yet working draft for handling waypoint trigger events

--- sim_controller.py
+++ sim_controller.py
@@ -5,6 +5,72 @@ from beamngpy import Scenario
 from db_types import TestCase, Participant
 
 
+def add_trigger_for_changing_waypoint() -> None:
+    from app import app
+    from common import add_to_prefab_file
+    import os
+    lua_file_path = os.path.join(
+        app.config["BEAMNG_USER_PATH"],
+        "levels",
+        app.config["BEAMNG_LEVEL_NAME"],
+        "scenarios",
+        app.config["BEAMNG_SCENARIO_NAME"] + ".lua"
+    )
+    lua_file = open(lua_file_path, "w")
+    lua_file.writelines([
+        "local M = {}\n",
+        "map = require(\"map\")\n",
+        "\n",
+        "local function onWaypoint(args)\n",
+        "  log('I', 'Test','onWaypoint called ')\n",
+        "end\n",
+        "\n",
+        "local function onBeamNGWaypoint(args)\n",
+        "  log('I', 'Test','onBeamNGWaypoint called ')\n",
+        "end\n",
+        "\n",
+        "local function onRaceWaypointReached(args)\n",
+        "  log('I', 'Test','onRaceWaypointReached called ')\n",
+        "end\n",
+        "\n",
+        "local function onRaceStart()\n",
+        "  log('I', 'Test','onRaceStart called ')\n",
+        "end\n",
+        "\n",
+        "local function onBeamNGTrigger()\n",
+        "  log('I', 'Test','onBeamNGTrigger called ')\n",
+        "end\n",
+        "\n",
+        "M.onWaypoint = onWaypoint\n",
+        "M.onBeamNGWaypoint = onBeamNGWaypoint\n",
+        "M.onRaceWaypointReached = onRaceWaypointReached\n",
+        "M.onRaceStart = onRaceStart\n",
+        "M.onBeamNGTrigger = onBeamNGTrigger\n",
+        "return M"
+    ])
+
+    add_to_prefab_file([
+        "new BeamNGTrigger(pierTrigger) {",
+        "    TriggerType = \"Sphere\";",
+        "    TriggerMode = \"Overlaps\";",
+        "    TriggerTestType = \"Bounding Box\";",
+        "    luaFunction = \"onBeamNGTrigger\";",
+        "    tickPeriod = \"100\";",
+        "    debug = \"0\";",
+        "    ticking = \"0\";",
+        "    triggerColor = \"255 192 0 45\";",
+        # "    cameraOnEnter = \"pierCamera\";",
+        "    defaultOnLeave = \"1\";",
+        "    position = \"10 0 0\";",
+        "    scale = \"20 20 20\";",
+        "    rotationMatrix = \"0 0 0 0 0 0 0 0 0\";",
+        "    mode = \"Ignore\";",
+        "    canSave = \"1\";",
+        "    canSaveDynamicFields = \"1\";",
+        "};"
+    ])
+
+
 def add_movements_to_scenario(participants: List[Participant], scenario: Scenario) -> None:
     """
     Makes cars move. Must be called after starting the scenario.
@@ -42,6 +108,7 @@ def run_test_case(test_case: TestCase):
     bng_scenario = Scenario(app.config["BEAMNG_LEVEL_NAME"], app.config["BEAMNG_SCENARIO_NAME"], authors=authors)
     test_case.scenario.add_all(bng_scenario)
     bng_scenario.make(bng_instance)
+    add_trigger_for_changing_waypoint()
     # FIXME As long as manually inserting text it can only be called after make
     test_case.scenario.add_waypoints_to_scenario(bng_scenario)
     bng_instance.open(launch=True)
