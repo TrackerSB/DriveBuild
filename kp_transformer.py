7755ef349dc8fb19c10f20ed72aab7de4cae14d9
Stefan Huber <huberst@fim.uni-passau.de>
2019-05-15 10:11:54 +0200

    drivebuild: Separate criteria types

--- kp_transformer.py
+++ kp_transformer.py
@@ -1,8 +1,6 @@
-from abc import ABC, abstractmethod
-
 from lxml.etree import _ElementTree
 
-from db_types import Criteria
+from criteria_types import Criteria
 
 
 def generate_criteria(crit_def: _ElementTree) -> Criteria:
