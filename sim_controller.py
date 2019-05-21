928f9723e2b6d6bd259d217ede769b33a027e0a1
Stefan Huber <huberst@fim.uni-passau.de>
2019-05-21 13:34:03 +0200

    drivebuild: Add FIXME about recognizing speeds

--- sim_controller.py
+++ sim_controller.py
@@ -56,6 +56,7 @@ def enable_participant_movements(participants: List[Participant]) -> None:
                     "  if data['event'] == 'enter' then"
                 ])
                 if waypoint.mode is MovementMode.MANUAL:
+                    # FIXME Recognize speed (limits)
                     lua_lines.extend([
                         "    sh.setAiRoute('" + participant.id + "', " + remaining_waypoints + ")"
                     ])
