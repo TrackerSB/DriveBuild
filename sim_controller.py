beec7b9d104bdbd08ab7edc01bd4daa5524f6f7d
Stefan Huber <huberst@fim.uni-passau.de>
2019-05-29 09:01:30 +0200

    drivebuild: Split methods and follow python naming

--- sim_controller.py
+++ sim_controller.py
@@ -1,13 +1,13 @@
-from typing import List, Set
+from typing import List, Set, Optional
 
 from beamngpy import Scenario
 
 from dbtypes.beamng import DBVehicle
 from dbtypes.criteria import TestCase
-from dbtypes.scheme import Participant
+from dbtypes.scheme import Participant, MovementMode
 
 
-def get_movement_mode_file_path(participant: Participant) -> str:
+def _get_movement_mode_file_path(participant: Participant) -> str:
     """
     Returns the path of the file for storing the current movement mode of the given participant. The used path separator
     is '/' to allow to be used in lua files.
@@ -17,39 +17,33 @@ def get_movement_mode_file_path(participant: Participant) -> str:
     return participant.id + "_movementMode"
 
 
-def enable_participant_movements(participants: List[Participant]) -> None:
+def _generate_lua_av_command(participant: Participant, idx: int, next_mode: MovementMode,
+                             current_mode: Optional[MovementMode] = None) -> List[str]:
     """
-    Adds triggers to the scenario that set the next waypoints for the given participants. Must be called after adding
-    the waypoints. Otherwise some IDs of waypoints may be None.
-    :param participants: The participants to add movement changing triggers to
+    NOTE When using this function the lua file where you include this command has to include the following line:
+    local sh = require('ge/extensions/scenario/scenariohelper')
     """
-    from util import add_to_prefab_file, eprint, get_lua_path
-    from dbtypes.scheme import MovementMode
-    from typing import Optional
+    from util import eprint
+    lua_av_command = []
+    if next_mode is MovementMode.MANUAL:
+        # FIXME Recognize speed (limits)
+        if current_mode is not MovementMode.MANUAL:
+            remaining_waypoints = "{'" + "', '".join(map(lambda w: w.id, participant.movement[idx + 1:])) + "'}"
+            lua_av_command.extend([
+                "    sh.setAiRoute('" + participant.id + "', " + remaining_waypoints + ")"
+            ])
+    else:
+        eprint("Mode " + str(next_mode) + " not supported, yet.")
+    lua_av_command.extend([
+        "    local modeFile = io.open('" + _get_movement_mode_file_path(participant) + "', 'w')",
+        "    modeFile:write('" + next_mode.value + "')",
+        "    modeFile:close()"
+    ])
+    return lua_av_command
 
-    def generate_lua_av_command(participant: Participant, idx: int, next_mode: MovementMode,
-                                current_mode: Optional[MovementMode] = None) -> List[str]:
-        """
-        NOTE When using this function the lua file where you include this command has to include the following line:
-        local sh = require('ge/extensions/scenario/scenariohelper')
-        """
-        lua_av_command = []
-        if next_mode is MovementMode.MANUAL:
-            # FIXME Recognize speed (limits)
-            if current_mode is not MovementMode.MANUAL:
-                remaining_waypoints = "{'" + "', '".join(map(lambda w: w.id, participant.movement[idx + 1:])) + "'}"
-                lua_av_command.extend([
-                    "    sh.setAiRoute('" + participant.id + "', " + remaining_waypoints + ")"
-                ])
-        else:
-            eprint("Mode " + str(next_mode) + " not supported, yet.")
-        lua_av_command.extend([
-            "    local modeFile = io.open('" + get_movement_mode_file_path(participant) + "', 'w')",
-            "    modeFile:write('" + next_mode.value + "')",
-            "    modeFile:close()"
-        ])
-        return lua_av_command
 
+def _generate_lua_file(participants: List[Participant]) -> None:
+    from util import get_lua_path
     lua_file = open(get_lua_path(), "w")
     lua_file.writelines([  # FIXME Is this needed somehow?
         "local M = {}\n",
@@ -60,7 +54,7 @@ def enable_participant_movements(participants: List[Participant]) -> None:
     ])
     for participant in participants:
         for idx, waypoint in enumerate(participant.movement[0:1]):
-            lua_file.writelines(generate_lua_av_command(participant, idx, waypoint.mode))
+            lua_file.writelines(_generate_lua_av_command(participant, idx, waypoint.mode))
     lua_file.writelines([
         "end\n",
         "\n",
@@ -68,6 +62,9 @@ def enable_participant_movements(participants: List[Participant]) -> None:
         "return M"
     ])
 
+
+def _add_lua_triggers(participants: List[Participant]) -> None:
+    from util import add_to_prefab_file
     for participant in participants:
         current_movement_mode = None
         for idx, waypoint in enumerate(participant.movement[1:-1]):
@@ -83,7 +80,7 @@ def enable_participant_movements(participants: List[Participant]) -> None:
                     "local function onWaypoint(data)",
                     "  if data['event'] == 'enter' then"
                 ])
-                lua_lines.extend(generate_lua_av_command(participant, idx, waypoint.mode, current_movement_mode))
+                lua_lines.extend(_generate_lua_av_command(participant, idx, waypoint.mode, current_movement_mode))
                 lua_lines.extend([
                     "  end",
                     "end",
@@ -115,7 +112,17 @@ def enable_participant_movements(participants: List[Participant]) -> None:
             current_movement_mode = waypoint.mode
 
 
-def make_lanes_visible() -> None:
+def _enable_participant_movements(participants: List[Participant]) -> None:
+    """
+    Adds triggers to the scenario that set the next waypoints for the given participants. Must be called after adding
+    the waypoints. Otherwise some IDs of waypoints may be None.
+    :param participants: The participants to add movement changing triggers to
+    """
+    _generate_lua_file(participants)
+    _add_lua_triggers(participants)
+
+
+def _make_lanes_visible() -> None:
     """
     Workaround for making lanes visible.
     """
@@ -136,7 +143,7 @@ def make_lanes_visible() -> None:
     prefab_file.close()
 
 
-def control_avs(vehicles: List[DBVehicle]) -> None:
+def _control_avs(vehicles: List[DBVehicle]) -> None:
     from beamngpy import socket
     # TODO Check which AVs are in AUTONOMOUS or TRAINING mode
     # TODO Request AIs for request ids to get data for
@@ -157,7 +164,7 @@ def control_avs(vehicles: List[DBVehicle]) -> None:
             print()
 
 
-def add_lap_config(waypoint_ids: Set[str]) -> None:
+def _add_lap_config(waypoint_ids: Set[str]) -> None:
     """
     Adds a dummy lapConfig attribute to the scenario json to avoid nil value exceptions.
     """
@@ -188,16 +195,16 @@ def run_test_case(test_case: TestCase):
     bng_scenario.make(bng_instance)
 
     # Make manual changes to the scenario files
-    make_lanes_visible()
+    _make_lanes_visible()
     # FIXME As long as manually inserting text it can only be called after make
     test_case.scenario.add_waypoints_to_scenario(bng_scenario)
-    enable_participant_movements(test_case.scenario.participants)
+    _enable_participant_movements(test_case.scenario.participants)
     waypoints = set()
     for wps in [p.movement for p in test_case.scenario.participants]:
         for wp in wps:
             if wp.id is not None:  # FIXME Not all waypoints are added
                 waypoints.add(wp.id)
-    add_lap_config(waypoints)
+    _add_lap_config(waypoints)
 
     bng_instance.open(launch=True)
     try:
@@ -225,7 +232,7 @@ def run_test_case(test_case: TestCase):
                 # test_case_result = "undetermined"
                 for vehicle in vehicles:
                     bng_instance.poll_sensors(vehicle)  # Update sensor cache before controlling AVs
-                control_avs(vehicles)
+                _control_avs(vehicles)
                 bng_instance.step(test_case.aiFrequency)
         print("Test case result: " + test_case_result)
     finally:
