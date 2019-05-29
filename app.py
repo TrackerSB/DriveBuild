2bc6519de1504fa8d0a1f791e6c0c73d09d053f8
Stefan Huber <huberst@fim.uni-passau.de>
2019-05-29 14:46:18 +0200

    drivebuild: Use double quotes

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_pyfile('app.cfg')
+app.config.from_pyfile("app.cfg")
 
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def test_launcher():
     from flask import render_template, request, flash, redirect
     from tc_manager import run_tests
-    input_field_name = 'testInput'
-    if request.method == 'POST':
+    input_field_name = "testInput"
+    if request.method == "POST":
         # check if the post request has the file part
         if input_field_name not in request.files:
-            flash('No file part')
+            flash("No file part")
             return redirect(request.url)
         file = request.files[input_field_name]
         # if user does not select file, browser also submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
+        if file.filename == "":
+            flash("No selected file")
             return redirect(request.url)
         if file and allowed_file(file.filename):
             run_tests(file)
@@ -32,13 +32,13 @@ def test_launcher():
 @app.errorhandler(404)
 def page_not_found(error):
     from flask import render_template
-    return render_template('error404.html', error=error), 404
+    return render_template("error404.html", error=error), 404
 
 
 @app.errorhandler(500)
 def page_not_found(error):
     from flask import render_template
-    return render_template('error500.html', error=error), 500
+    return render_template("error500.html", error=error), 500
 
 
 @app.errorhandler(501)
@@ -51,6 +51,6 @@ def start_ai_exchange_service() -> None:
     pass  # TODO Implement starting AIExchangeService
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
     start_ai_exchange_service()
