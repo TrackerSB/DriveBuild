f6c52e3eee47856c352b9cb926c72d3ddc3846e8
TrackerSB <huberst@fim.uni-passau.de>
2019-10-11 15:01:21 +0200

    drivebuild: Remove superfluous locks

--- dbtypes/beamngpy.py
+++ dbtypes/beamngpy.py
@@ -21,34 +21,24 @@ class DBBeamNGpy(BeamNGpy):
         Path(user_path).mkdir(parents=True, exist_ok=True)
         super().__init__(host, port, BEAMNG_INSTALL_FOLDER, user_path)
         self.current_tick = 0
-        self.instance_lock = Lock()
 
     def step(self, count, wait=True):
-        self.instance_lock.acquire()
         super().step(count, wait)
-        self.instance_lock.release()
         self.current_tick += count
 
     def poll_sensors(self, vehicle):
-        self.instance_lock.acquire()
         if self.skt:
             try:
                 super().poll_sensors(vehicle)
             except Exception as ex:
                 raise BeamNGpyException("Polling sensors failed") from ex
-            finally:
-                self.instance_lock.release()
-        else:
-            self.instance_lock.release()
 
     def close(self):
-        self.instance_lock.acquire()
         try:
             super().close()
             DBBeamNGpy.user_path_pool.put(self.user)
         except Exception as ex:
             raise BeamNGpyException("Closing BeamNG failed") from ex
-        self.instance_lock.release()
 
 
 class BeamNGpyException(RuntimeWarning):
