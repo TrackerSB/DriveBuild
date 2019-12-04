01393f3e8720397f5755da9b2409cb8420e89c26
Stefan Huber <stefan.huber.niedling@outlook.com>
2019-12-04 17:18:14 +0100

    level: Reorganize and nailcare

--- levels/drivebuild/art/materials.cs
+++ levels/drivebuild/art/materials.cs
@@ -2,7 +2,9 @@ singleton Material(groundmesh_grass2)
 {
     mapTo = "groundmesh_grass2";
     vertColor[0] = "1";
-    diffuseMap[0] = "levels/drivebuild/art/shapes/roads/Overlay_Grass-01.dds";
+    diffuseMap[0] = "levels/drivebuild/art/Overlay_Grass-01.dds";
+    detailMap[0] = "levels/drivebuild/art/Grass-01-D.dds";
+    normalMap[0] = "levels/drivebuild/art/Grass-01-N.dds";
     specularColor[0] = "0 0 0";
     specularPower[0] = "32";
     pixelSpecular[0] = "0";
@@ -13,7 +15,8 @@ singleton Material(groundmesh_grass2)
     translucentBlendOp = "None";
     alphaTest = "1";
     alphaRef = "64";
-    materialTag0 = "beamng"; materialTag1 = "vehicle";
+    materialTag0 = "beamng";
+    materialTag1 = "vehicle";
     detailScale[0] = "4 0.5";
     annotation = "NATURE";
 };
