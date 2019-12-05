bef40b8168d56608fa22f73461d6278f93fd7832
Stefan Huber <stefan.huber.niedling@outlook.com>
2019-12-05 10:59:38 +0100

    client: Precise setup.py

--- client/setup.py
+++ client/setup.py
@@ -6,15 +6,16 @@ with open(os.path.join(os.path.dirname(__file__), "drivebuildclient/README.md"),
 
 setuptools.setup(
     name="drivebuild-client",
-    version="0.38",
+    version="0.39",
     author="Stefan Huber",
     author_email="huberst@fim.uni-passau.de",
-    description="A client for accessing a DriveBuild server.",
+    description="A client for accessing a DriveBuild MainApp.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(
         include=("drivebuildclient",)
     ),
+    python_requires=">=3.6",
     install_requires=[
         "dill",
         "flask",
