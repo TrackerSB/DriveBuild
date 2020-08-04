448206ac836f16574f27dff0eb8c93bcd4c4110d
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-08-04 19:27:18 +0200

    Client: State explicitly Python 3.8 support

--- client/setup.py
+++ client/setup.py
@@ -28,6 +28,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     project_urls={
         "Source": "https://github.com/TrackerSB/DriveBuild"
